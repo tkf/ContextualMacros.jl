6dc9a16307ce962b0705e63c041b4cf279bfa969
Takafumi Arakaki <aka.tkf@gmail.com>
2020-08-06 15:19:23 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.6

---
+++ src/ContextualMacros.jl
@@ -0,0 +1,5 @@
+module ContextualMacros
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ContextualMacros
+using Test
+
+@testset "ContextualMacros.jl" begin
+    # Write your tests here.
+end
